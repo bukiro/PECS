cce5766a76a087fe82e5f86ba64cba51651e5364
bukiro <arne.x.brandt@outlook.com>
2021-09-09 11:06:52 +0200

    Improved logging and error handling in standalone

--- publish/pecs.js
+++ publish/pecs.js
@@ -4,23 +4,37 @@ var https = require('https');
 var fs = require('fs');
 
 var logFile = __dirname + "/pecs.log";
-function log(message, withDate = true, die = false) {
+function log(message, withDate = true, error = false, die = false) {
     date = new Date();
     var day = ("0" + date.getDate()).slice(-2);
     var month = ("0" + (date.getMonth() + 1)).slice(-2);
     var year = date.getFullYear();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
+    var hours = ("0" + date.getHours()).slice(-2);
+    var minutes = ("0" + date.getMinutes()).slice(-2);
+    var seconds = ("0" + date.getSeconds()).slice(-2);
     var dateStr = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
     var dateMessage = "\n" + message;
     if (withDate) {
         dateMessage = "\n" + dateStr + ":: " + message;
     }
     fs.appendFileSync(logFile, dateMessage);
-    console.log(message);
+    if (error) {
+        console.error(message);
+    } else {
+        console.log(message);
+    }
     if (die) {
-        process.exit(1);
+        console.log('Press any key to exit');
+        const keypress = async () => {
+            process.stdin.setRawMode(true)
+            return new Promise(resolve => process.stdin.once('data', () => {
+                process.stdin.setRawMode(false)
+                resolve()
+            }))
+        }
+        (async () => {
+            await keypress()
+        })().then(process.exit)
     }
 }
 
@@ -30,66 +44,99 @@ const app = express();
 fs.readFile('./config.json', 'utf8', function (err, data) {
     if (err) {
         log('config.json was not found or could not be opened: ');
-        log(err, true, true);
-    }
-    var config = JSON.parse(data);
+        log(err, true, true, true);
+    } else {
+        var config = JSON.parse(data);
 
-    dir = __dirname + "/src";
-    app.use(express.static(dir));
+        dataDir = __dirname + "/src";
+        app.use(express.static(dataDir));
 
-    var dbConnectionURL = config.dbConnectionURL || "";
-    var HTTPPort = config.HTTPPort || 4200;
-    var HTTPSPort = config.HTTPSPort || 4443;
-    var SSLCertificatePath = config.SSLCertificatePath || "";
-    var SSLPrivateKeyPath = config.SSLPrivateKeyPath || "";
+        var dbConnectionURL = config.dbConnectionURL || "";
+        var HTTPPort = config.HTTPPort || 4200;
+        var HTTPSPort = config.HTTPSPort || 4443;
+        var SSLCertificatePath = config.SSLCertificatePath || "";
+        var SSLPrivateKeyPath = config.SSLPrivateKeyPath || "";
 
-    if (dbConnectionURL) {
-        log("Preparing PECS config file in src/assets/config.json")
-        fs.writeFileSync("src/assets/config.json", JSON.stringify({ dbConnectionURL: dbConnectionURL }), function (err) {
-            if (err) {
-                log("Could not prepare PECS config file: ");
-                log(err);
+        if (dbConnectionURL) {
+            log("Preparing PECS config file in src/assets/config.json")
+            fs.writeFileSync("src/assets/config.json", JSON.stringify({ dbConnectionURL: dbConnectionURL }), function (err) {
+                if (err) {
+                    log("Could not prepare PECS config file: ");
+                    log(err);
+                }
+            });
+        }
+
+        async function startHTTP() {
+            var httpServer = http.createServer(app);
+            try {
+                await new Promise((resolve, reject) => {
+                    httpServer.listen(HTTPPort, () => {
+                        log('HTTP server is listening on port ' + HTTPPort);
+                        resolve();
+                    });
+                    httpServer.once('error', (err) => {
+                        reject(err);
+                    });
+                });
+                return;
+            } catch (err) {
+                log("HTTP server could not be started: ");
+                log(err, true, true, true);
             }
-        });
-    }
-
-    var httpServer = http.createServer(app)
-    httpServer.listen(HTTPPort, () => {
-        log('HTTP server is running on port ' + HTTPPort)
-    })
-
-    if (SSLCertificatePath && SSLPrivateKeyPath) {
-        try {
-            var certificate = fs.readFileSync(SSLCertificatePath, 'utf8');
-        } catch (err) {
-            log('SSL certificate not found at ' + SSLCertificatePath)
-            certificate = "";
         }
-        try {
-            var privateKey = fs.readFileSync(SSLPrivateKeyPath, 'utf8');
-        } catch (err) {
-            log('SSL private key not found at ' + SSLPrivateKeyPath)
-            privateKey = "";
-        }
-        if (certificate && privateKey) {
-            var credentials = { key: privateKey, cert: certificate };
-            var httpsServer = https.createServer(credentials, app);
-            httpsServer.listen(HTTPSPort, () => {
-                log('HTTPS server listening on port ' + HTTPSPort)
-            })
-        } else {
+        startHTTP()
+
+        if (SSLCertificatePath && SSLPrivateKeyPath) {
+            try {
+                var certificate = fs.readFileSync(SSLCertificatePath, 'utf8');
+            } catch (err) {
+                log('SSL certificate not found at ' + SSLCertificatePath)
+                certificate = "";
+            }
+            try {
+                var privateKey = fs.readFileSync(SSLPrivateKeyPath, 'utf8');
+            } catch (err) {
+                log('SSL private key not found at ' + SSLPrivateKeyPath)
+                privateKey = "";
+            }
+            if (certificate && privateKey) {
+                var credentials = { key: privateKey, cert: certificate };
+                
+                async function startHTTPS() {
+                    var httpsServer = https.createServer(credentials, app);
+
+                    try {
+                        await new Promise((resolve, reject) => {
+                            httpsServer.listen(HTTPSPort, () => {
+                                log('HTTPS server is listening on port ' + HTTPSPort);
+                                resolve();
+                            });
+                            httpsServer.once('error', (err) => {
+                                reject(err);
+                            });
+                        });
+                        return;
+                    } catch (err) {
+                        log("HTTPS server could not be started: ");
+                        log(err, true, true, true);
+                    }
+                }
+                startHTTPS()
+            } else {
+                log('HTTPS server was not started.')
+            }
+
+        } else if (SSLCertificatePath || SSLPrivateKeyPath) {
+            log('SSL information missing from config.json: ')
+            if (!SSLCertificatePath) {
+                log(' SSLCertificatePath');
+            }
+            if (!SSLPrivateKeyPath) {
+                log(' SSLPrivateKeyPath');
+            }
             log('HTTPS server was not started.')
         }
-
-    } else if (SSLCertificatePath || SSLPrivateKeyPath) {
-        log('SSL information missing from config.json: ')
-        if (!SSLCertificatePath) {
-            log(' SSLCertificatePath');
-        }
-        if (!SSLPrivateKeyPath) {
-            log(' SSLPrivateKeyPath');
-        }
-        log('HTTPS server was not started.')
     }
 
 })
