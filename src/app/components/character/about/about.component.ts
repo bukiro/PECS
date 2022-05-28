e567e3cce300db86af1295d30c2b58b5276e75f8
Arne Brandt <arne.x.brandt@outlook.com>
2022-05-28 17:03:48 +0200

    Fix lint errors in AboutComponent

--- src/app/components/character/about/about.component.ts
+++ src/app/components/character/about/about.component.ts
@@ -1,43 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import welcome from 'src/assets/json/about/welcome.json';
 import hints from 'src/assets/json/about/hints.json';
 import changelog from 'src/assets/json/about/changelog.json';
 import { ConfigService } from 'src/app/services/config.service';
+import { Trackers } from 'src/libs/shared/util/trackers';
+
+interface ChangeLog {
+    version: string;
+    changes: Array<{ header: string; desc: string; images: Array<{ file: string; title: string }> }>;
+}
 
 @Component({
     selector: 'app-about',
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent implements OnInit {
 
     public version = '';
 
-    welcome: Array<{ header: string; desc: string }> = welcome;
-    hints: Array<{ header: string; desc: string; images: Array<{ file: string; title: string }> }> = hints;
-    changelog: Array<{ version: string; changes: Array<{ header: string; desc: string; images: Array<{ file: string; title: string }> }> }> = changelog;
+    public welcome: Array<{ header: string; desc: string }> = welcome;
+    public hints: Array<{ header: string; desc: string; images: Array<{ file: string; title: string }> }> = hints;
+    public changelog: Array<ChangeLog> = changelog;
 
     constructor(
-        private readonly configService: ConfigService,
+        private readonly _configService: ConfigService,
+        public trackers: Trackers,
     ) { }
 
-    trackByIndex(index: number): number {
-        return index;
+    public versionShown(): ChangeLog {
+        return this.changelog.find(versionChange => versionChange.version === this.version);
     }
 
-    get_VersionShown() {
-        return this.changelog.find(versionChange => versionChange.version == this.version);
-    }
+    public updateAvailable(): { available: boolean; desc: string } {
+        const updateAvailable = this._configService.updateAvailable;
 
-    get_UpdateAvailable() {
-        const updateAvailable = this.configService.updateAvailable();
-
-        if (updateAvailable == 'n/a') {
-            return [{ available: false, desc: 'PECS was unable to check for new versions.' }];
+        if (updateAvailable === 'n/a') {
+            return { available: false, desc: 'PECS was unable to check for new versions.' };
         } else if (updateAvailable) {
-            return [{ available: true, desc: `Version ${ updateAvailable } is available for download!` }];
+            return { available: true, desc: `Version ${ updateAvailable } is available for download!` };
         } else {
-            return [{ available: false, desc: '' }];
+            return { available: false, desc: '' };
         }
     }
 
