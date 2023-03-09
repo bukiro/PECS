d95074c8d08dead6696d688b7ef5894dc6b6fa7f
bukiro <arne.x.brandt@outlook.com>
2023-03-09 23:26:47 +0100

    Create logo

---
+++ src/libs/shared/ui/logo/components/logo/logo.component.ts
@@ -0,0 +1,20 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { BaseClass } from 'src/libs/shared/util/mixins/base-class';
+import { TrackByMixin } from 'src/libs/shared/util/mixins/track-by-mixin';
+
+@Component({
+    selector: 'app-logo',
+    templateUrl: './logo.component.html',
+    styleUrls: ['./logo.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LogoComponent extends TrackByMixin(BaseClass) {
+
+    public letters = [
+        { letter: 'P', checked: false },
+        { letter: 'E', checked: true },
+        { letter: 'C', checked: false },
+        { letter: 'S', checked: false },
+    ];
+
+}
